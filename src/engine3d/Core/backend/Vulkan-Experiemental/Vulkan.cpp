721514800eaf5d7f419a8af4c10676725297dcc2
SpinnerX <heraaronhotmail@yahoo.com>
2024-09-12 15:11:37 -0700

    Updated includes for Core/core.h

--- src/engine3d/Core/backend/Vulkan-Experiemental/Vulkan.cpp
+++ src/engine3d/Core/backend/Vulkan-Experiemental/Vulkan.cpp
@@ -3,12 +3,17 @@
 #include <Core/backend/utilities/helper_functions.h>
 #include <vulkan/vulkan_core.h>
 
+#if __WIN32
 #define VK_USE_PLATFORM_WIN32_KHR
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #define GLFW_EXPOSE_NATIVE_WIN32
 #include <GLFW/glfw3native.h>
 #include <vulkan/vulkan.h>
+#else
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+#endif
 
 namespace engine3d::vk{
     // namespace vk{
