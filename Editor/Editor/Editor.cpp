1dee1e7325d5c973545bd6c0ce74aaaf20aa3046
SpinnerX <heraaronhotmail@yahoo.com>
2024-09-12 14:39:29 -0700

    Fixed lots of git conflicts

--- Editor/Editor/Editor.cpp
+++ Editor/Editor/Editor.cpp
@@ -8,7 +8,6 @@
 namespace engine3d{
 
     EditorApplication::EditorApplication(const std::string& p_DebugName) : ApplicationInstance(p_DebugName) {
-<<<<<<< HEAD
         Renderer::Initialize();
         Renderer::SetBackgroundColor({1.0f, 0.0f, 0.0f, 0.0f});
     }
@@ -35,37 +34,6 @@ namespace engine3d{
         return new EditorApplication();
     }
 
-=======
-        /* Renderer::Initialize(); */
-        /* Renderer::SetBackgroundColor({0.0f, 0.5f, 0.5f, 0.f}); */
-        Renderer::SetBackgroundColor({1.0f, 0.0f, 0.0f, 0.0f});
-
-        ConsoleLogInfo("Initializing Current Application Name === {}!!!", ApplicationInstance::CurrentApplicationName());
-    }
-
-    EditorApplication::~EditorApplication() {}
-
-    void EditorApplication::UpdateThisApplicationInstance(){
-        
-        //! @note Just testing to see if application still closes cleanly.
-        // if(InputPoll::IsKeyPressed(ENGINE_KEY_ESCAPE)){
-        //     exit(0);
-        // }
-
-        //! @note This function will render our primitives
-        //! @note TODO --  Flush should only happens when our scene is given everything that lives within this scene (ref to lifetimes)
-        /* Renderer::FlushScene(); */
-		
-		Renderer::Presentation();
-        // ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
-        // ImGui::End();
-    }
-
-    ApplicationInstance* InitializeApplication(){
-        return new EditorApplication();
-    }
-
->>>>>>> fa59aaf07c9cbf80f5d25918c4d43b3cca25238f
 
 
 };
