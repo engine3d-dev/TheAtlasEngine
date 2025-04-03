de598614b20b21f9430aed959ebae22a4250e8eb
= <=>
2025-04-03 15:16:47 -0700

    Reformatted physics code to not violate clang-format

--- src/atlas/physics/physics_3d/physics_api.cpp
+++ src/atlas/physics/physics_3d/physics_api.cpp
@@ -1,25 +1,21 @@
 #include <physics/physics_3d/jolt/jolt_physics_engine.hpp>
 #include <core/scene/scene_object.hpp>
 
-
 namespace atlas::physics {
 
-
     void create(ref<scene_object> physics_object) {
-        
-        if(physics_object == nullptr) {
+
+        if (physics_object == nullptr) {
             console_log_fatal("Missing physics scene object.");
             return;
         }
 
-        if(physics_object->get<jolt_settings>() == nullptr) {
+        if (physics_object->get<jolt_settings>() == nullptr) {
             console_log_fatal("Missing jolt_settings component.");
             return;
         }
-        
-        create_engine(*physics_object->get<jolt_settings>());
-        
 
+        create_engine(*physics_object->get<jolt_settings>());
     }
 
 }
