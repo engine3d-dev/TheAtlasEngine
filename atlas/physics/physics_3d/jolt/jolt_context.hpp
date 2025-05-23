75529bf455aeb47048eb439540fc2bd0c7b3eb9d
ZacharyH777 <zacharyh777@gmail.com>
2025-05-23 09:45:36 -0700

    Comments for classes jolt_context and jolt_broad_phase
    This is here to ensure that the classes or well organized and are prepared for doxygen. As well as to determine what still needs to be done, both now and later.

--- atlas/physics/physics_3d/jolt/jolt_context.hpp
+++ atlas/physics/physics_3d/jolt/jolt_context.hpp
@@ -7,28 +7,66 @@
 #include <physics/physics_3d/jolt/interface/jolt_broad_phase.hpp>
 
 namespace atlas::physics {
-    
+
     class jolt_context : public physics_context {
-        public:
-            jolt_context(jolt_settings p_settings);
-            ~jolt_context() override;
-        private:
-            void engine_create_physics_bodies() override;
-            void engine_clean_physics_bodies() override;
-            void engine_run_physics_step() override;
+    public:
+        jolt_context(jolt_settings p_settings);
+        ~jolt_context() override;
 
-            void store_shape(uint64_t id, const JPH::RefConst<JPH::Shape>& shape);
+    private:
+        /**
+         * @brief Creates all of the physics bodies at the start of runtime.
+         * Utilizing the main scene provided in flecs to establish what to load.
+         *
+         * @warning Main scene is not defined and therefore will only utilzed
+         * level_scene as of right now. In order to fix this issue a
+         * get_main_scene() or get_main_scenes() must be created to develop a
+         * more dynamic system.
+         */
+        void engine_create_physics_bodies() override;
 
-            jolt_settings m_settings;
+        /**
+         * @brief Removes all physics bodies and shapes from level_scene.
+         *
+         * @warning Similar to create this only works with level_scene as of
+         * right now.
+         *
+         */
+        void engine_clean_physics_bodies() override;
 
-            ref<JPH::TempAllocatorImpl> m_temp_allocator;
-            ref<JPH::PhysicsSystem> m_physics_system;
-            scope<JPH::JobSystemThreadPool> m_thread_system;
+        /**
+         * @brief Runs the actual physics. Can be called multiple times in one
+         * frame. Runs the all calculation in physics. Threads must be defined
+         * in the jolt_config in order for this to work correctly.
+         *
+         */
+        void engine_run_physics_step() override;
 
-            ref<broad_phase_layer_interface> m_broad_phase_layer_interface;
-            ref<object_vs_broadphase_layer> m_object_vs_broadphase_filter;
-            ref<object_layer_pair_filter> m_object_layer_pair_filter;
+        /**
+         * @brief Jolt enforces the system to handle the shapes created instead
+         * of itself. Otherwise the ref will reach 0 and the object will crash
+         * as the shape is removed from the bopy. So a map was created to
+         * maintain the shapes scope.
+         *
+         * @param id shape id to be stored in map.
+         * @param shape the shape that was created.
+         */
+        void store_shape(uint64_t id, const JPH::RefConst<JPH::Shape>& shape);
 
-            std::unordered_map<uint64_t, JPH::RefConst<JPH::Shape>> m_shape_registry;
+        //! @note Must be defined before physics can be initialized otherwise
+        //! jolt cannot be created properly.
+        jolt_settings m_settings;
+
+        ref<JPH::TempAllocatorImpl> m_temp_allocator;
+        ref<JPH::PhysicsSystem> m_physics_system;
+        scope<JPH::JobSystemThreadPool> m_thread_system;
+
+        ref<broad_phase_layer_interface> m_broad_phase_layer_interface;
+        ref<object_vs_broadphase_layer> m_object_vs_broadphase_filter;
+        ref<object_layer_pair_filter> m_object_layer_pair_filter;
+
+        //! @note This map is to create the scope for shapes created by Jolt.
+        std::unordered_map<uint64_t, JPH::RefConst<JPH::Shape>>
+          m_shape_registry;
     };
 };
