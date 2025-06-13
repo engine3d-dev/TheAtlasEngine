ee9497cb86e4540e74a06ab1f45525f933a414d6
SpinnerX <heraaronhotmail@yahoo.com>
2025-06-13 13:27:00 -0700

    Updated hash function for vertices in unordered_map

--- atlas/drivers/vulkan-cpp/hash.hpp
+++ atlas/drivers/vulkan-cpp/hash.hpp
@@ -1,30 +1,29 @@
 #pragma once
 #include <drivers/vulkan-cpp/vk_types.hpp>
-
-// #define GLM_ENABLE_EXPERIMENTAL
-// #include <glm/gtx/hash.hpp>
-
-// namespace std {
-
-//     /**
-//      * @brief Specialization for hash function for using vertex into the hash table
-//     */
-//     template<>
-//     struct hash<atlas::vk::vertex> {
-//         size_t operator()(atlas::vk::vertex const& vertex) const {
-//             return ((hash<glm::vec3>()(vertex.position) ^ (hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^ (hash<glm::vec3>()(vertex.normals) << 1)  ^ (hash<glm::vec2>()(vertex.uv) << 1);
-//         }
-//     };
-// }
-
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/hash.hpp>
 
+namespace atlas {
+    template<typename T, typename... Rest>
+    void hash_combine(size_t& seed, const T& v, const Rest&... rest) {
+        seed ^= std::hash<T>()(v) + 0x9e3779b9 + (seed << 6) + (seed << 2);
+        (hash_combine(seed, rest), ...);
+    }
+
+};
+
 namespace std {
+
     template<>
     struct hash<atlas::vk::vertex> {
-        size_t operator()(atlas::vk::vertex const& vertex) const {
-            return ((hash<glm::vec3>()(vertex.position) ^ (hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^ (hash<glm::vec3>()(vertex.normals) << 1)  ^ (hash<glm::vec2>()(vertex.uv) << 1);
+        size_t operator()(const atlas::vk::vertex& vertex) const {
+            size_t seed = 0;
+            atlas::hash_combine(seed,
+                                vertex.position,
+                                vertex.color,
+                                vertex.normals,
+                                vertex.uv);
+            return seed;
         }
     };
 }
