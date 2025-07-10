e7bc7073d9264dc82002668a3b41acc83efa1a12
ZacharyH777 <zacharyh777@gmail.com>
2025-07-09 18:35:18 -0700

    Deleted console log

--- src/atlas/physics/physics_3d/jolt/jolt_context.cpp
+++ src/atlas/physics/physics_3d/jolt/jolt_context.cpp
@@ -270,16 +270,14 @@ namespace atlas::physics {
 
         // Step the simulation
 
-        console_log_info("Application {}", application::delta_time());
-        int physics_step = 1 + (int)(60 * application::delta_time());
         if (!m_settings.use_fixed_timestep) {
+            int physics_step = 1 + (int)(60 * application::delta_time());
             m_physics_system->Update(application::delta_time(),
                                      physics_step,
                                      m_temp_allocator.get(),
                                      m_thread_system.get());
         }
         else {
-            console_log_info("Application {}", application::delta_time());
             int time_step = 1 + (int)(60 * m_settings.fixed_time_step);
             m_physics_system->Update(application::delta_time(),
                                      time_step,
