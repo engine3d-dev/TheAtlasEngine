53e638d975f794830704d0e186538ece6589d337
SpinnerX <heraaronhotmail@yahoo.com>
2025-08-26 21:52:12 -0700

    Forgot to add the updated scene_object.cpp alongside the previous commit

--- src/atlas/core/scene/scene_object.cpp
+++ src/atlas/core/scene/scene_object.cpp
@@ -8,6 +8,13 @@ namespace atlas {
         add<transform>();
     }
 
+    scene_object::scene_object(flecs::world* p_registry,
+                               const std::string& p_name,
+                               [[maybe_unused]] bool p_lookup) {
+        m_entity = p_registry->lookup(p_name.c_str());
+        add<transform>();
+    }
+
     scene_object::scene_object(strong_ref<flecs::world>& p_registry,
                                const std::string& p_name) {
         m_entity = p_registry->entity(p_name.c_str());
