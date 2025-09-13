4dc042c4f090710fc16dd520d6120acf88bbfe9d
SpinnerX <heraaronhotmail@yahoo.com>
2025-09-12 23:35:00 -0700

    Updated file dialog parameters for setting asset paths

--- src/atlas/core/filesystem/file_dialog.cpp
+++ src/atlas/core/filesystem/file_dialog.cpp
@@ -3,31 +3,33 @@
 #include <nfd/nfd.h>
 
 namespace atlas::filesystem {
-    std::string load_from_file_dialog(const std::string& p_filter) {
+    std::string load_from_file_dialog(const std::string& p_filter, const std::filesystem::path& p_default_path) {
         char* output_path = nullptr;
 
         nfdresult_t result =
-          NFD_OpenDialog(p_filter.c_str(), nullptr, &output_path);
+          NFD_OpenDialog(p_filter.c_str(), p_default_path.string().c_str(), &output_path);
 
-        if (result == NFD_OKAY) {
-            return std::string(output_path);
-        }
+        // if (result == NFD_OKAY) {
+        //     return std::string(output_path);
+        // }
 
-        console_log_fatal("Error Loading File!");
-        return "";
+        // console_log_fatal("Error Loading File!");
+        // return "";
+        return (result == NFD_OKAY) ? std::string(output_path) : "";
     }
 
-    std::string save_to_file([[maybe_unused]] const std::string& p_filter) {
+    std::string save_to_file(const std::string& p_filter, const std::filesystem::path& p_default_path) {
         char* output_path = nullptr;
 
         nfdresult_t result =
-          NFD_SaveDialog(p_filter.c_str(), nullptr, &output_path);
+          NFD_SaveDialog(p_filter.c_str(), p_default_path.string().c_str(), &output_path);
 
-        if (result == NFD_OKAY) {
-            return std::string(output_path);
-        }
+        // if (result == NFD_OKAY) {
+        //     return std::string(output_path);
+        // }
 
-        console_log_trace("Error Loading == {}", output_path);
-        return "";
+        // console_log_trace("Error Loading == {}", output_path);
+        // return "";
+        return (result == NFD_OKAY) ? std::string(output_path) : "";
     }
 };
