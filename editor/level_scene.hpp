00e81fda21157ace2f1a1bca4d3c60525411a9fc
SpinnerX <heraaronhotmail@yahoo.com>
2025-09-22 01:18:35 -0700

    Formatted level_scene to clang-format

--- editor/level_scene.hpp
+++ editor/level_scene.hpp
@@ -49,16 +49,18 @@ private:
     atlas::optional_ref<atlas::scene_object> m_camera;
 
     // Pre-setup that should be handled by atlas::physics_system
-    atlas::optional_ref<atlas::scene_object> m_physics_object_representation_of_settings;
-    atlas::optional_ref<atlas::physics::physics_engine> m_physics_engine_handler;
+    atlas::optional_ref<atlas::scene_object>
+      m_physics_object_representation_of_settings;
+    atlas::optional_ref<atlas::physics::physics_engine>
+      m_physics_engine_handler;
 
     // Should be hidden from initializing the physics system entirely
     std::pmr::polymorphic_allocator<> m_physics_system_allocator;
 
-    bool m_blink_text=false;
+    bool m_blink_text = false;
     glm::vec3 m_offset_from_camera;
 
-    bool m_physics_is_runtime=false;
+    bool m_physics_is_runtime = false;
 
     // Note -- Added this temporarily
     // ImFont* m_font;
