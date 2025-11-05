c405036ba96423e8389fa54194c583024dc2b78c
SpinnerX <heraaronhotmail@yahoo.com>
2025-11-05 10:49:36 -0800

    Added comments to applicaiton.hpp and core.hpp + removing a core_assert function that was not needed

--- atlas/core/core.hpp
+++ atlas/core/core.hpp
@@ -1,25 +1,24 @@
 #pragma once
-#include <cassert>
 #include <memory>
 #include <spdlog/common.h>
-#include <spdlog/spdlog.h>
 #include <core/utilities/memory.hpp>
 
 namespace atlas {
 
-    template<typename... T>
-    static void core_assert(bool x, T&&... args) {
-        if (!x) {
-            spdlog::critical("assertion failed: {}", std::forward<T>(args)...);
-        }
-    }
-
-    //! @brief alias to atlas::memory::strong_ptr<T>
+    /**
+     * @brief alias to atlas::memory::strong_ptr<T>
+    */
     template<typename T>
     using strong_ref = memory::strong_ptr<T>;
 
-    //! @return atlas::memory::make_strong_ptr<T> which constructs object of
-    //! type T
+    /**
+     * @brief construct strong_ptr<T> through atlas::memory::create_strong_ptr<T>(...);
+     * 
+     * @tparam ...Args is a template pack for packing in parameters
+     * @tparam T is the type of object to construct strong_ptr with
+     * @param p_allocator is a polymorphic allocator when creating strong_ptr's.
+     * @param ...Args is an argument pack for packing to deduce the types that is specified by object of type T
+    */
     template<typename T, typename... Args>
     strong_ref<T> create_strong_ref(
       std::pmr::polymorphic_allocator<> p_allocator,
@@ -28,24 +27,43 @@ namespace atlas {
                                           std::forward<Args>(args)...);
     }
 
+    /**
+     * @brief alias to memory::optional_ptr<T>
+    */
     template<typename T>
     using optional_ref = memory::optional_ptr<T>;
 
-    //! @brief alias to memory::optional_ptr<T>
+    /**
+     * @brief alias to std::shared_ptr<T>
+    */
     template<typename T>
     using ref = std::shared_ptr<T>;
 
-    //! @return constructed shared_ptr<T> object
+    /**
+     * @brief construct std::make_shared<T>(...);
+     * 
+     * @tparam ...Args is a template pack for packing in parameters
+     * @tparam T is the type of object to construct strong_ptr with
+     * @param ...Args is an argument pack for packing to deduce the types that is specified by object of type T
+    */
     template<typename T, typename... Args>
     constexpr ref<T> create_ref(Args&&... args) {
         return std::make_shared<T>(std::forward<Args>(args)...);
     }
 
-    //! @brief alias to unique_ptr<T>
+    /**
+     * @brief alias to std::unique_ptr<T>
+    */
     template<typename T>
     using scope = std::unique_ptr<T>;
 
-    //! @return make_unique<T> that constructs object
+    /**
+     * @brief construct std::make_unique<T>(...);
+     * 
+     * @tparam ...Args is a template pack for packing in parameters
+     * @tparam T is the type of object to construct strong_ptr with
+     * @param ...Args is an argument pack for packing to deduce the types that is specified by object of type T
+    */
     template<typename T, typename... Args>
     constexpr scope<T> create_scope(Args&&... args) {
         return std::make_unique<T>(std::forward<Args>(args)...);
