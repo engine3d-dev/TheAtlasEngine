839bc3830e912a27be209ce0788d6df4d023b0c1
SpinnerX <heraaronhotmail@yahoo.com>
2025-12-07 00:38:06 -0800

    Formatted code using clang-format

--- atlas/core/scene/exceptions.hpp
+++ atlas/core/scene/exceptions.hpp
@@ -13,7 +13,9 @@ namespace atlas {
           : m_block(p_data) {}
 
         //! @return message given when this exception gets triggered
-        [[nodiscard]] const char* what() const noexcept override { return m_block.data; }
+        [[nodiscard]] const char* what() const noexcept override {
+            return m_block.data;
+        }
 
     private:
         exception_block m_block;
