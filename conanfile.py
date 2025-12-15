362cd1824ff5c118a0340f10f0770f39741df230
SpinnerX <heraaronhotmail@yahoo.com>
2025-12-15 11:25:43 -0800

    Updated conanfile version to 0.f due to core API changes

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ import shutil
 
 class AtlasRecipe(ConanFile):
     name = "atlas"
-    version = "0.3"
+    version = "0.4"
     package_type = "library"
     license = "Apache-2.0"
     homepage = "https://github.com/engine3d-dev/TheAtlasEngine"
