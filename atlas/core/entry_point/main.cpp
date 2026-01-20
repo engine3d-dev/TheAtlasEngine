f199ecc0584f795725ea7a7029ea8203dfea4ea7
SpinnerX <heraaronhotmail@yahoo.com>
2026-01-20 01:25:50 -0800

    Conform code to clang-format

--- atlas/core/entry_point/main.cpp
+++ atlas/core/entry_point/main.cpp
@@ -4,68 +4,33 @@ import atlas.application;
 import atlas.common;
 import atlas.logger;
 import atlas.graphics_api;
-
-// expose visibility to ref<graphics>
 import atlas.drivers.graphics_context;
-
-// this includes initialize_context
-// import atlas.drivers;
 import atlas.renderer.context_loader;
 import atlas.core.scene.system_registry;
 
 // Defined in the user-application side
-[[nodiscard]] atlas::ref<atlas::application> initialize_application(atlas::ref<atlas::graphics_context> p_context);
+[[nodiscard]] atlas::ref<atlas::application> initialize_application(
+  atlas::ref<atlas::graphics_context> p_context);
 
-
-// /*
-// atlas/*
-//     core/*
-//         import atlas.core;
-//     drivers/
-//         vulkan
-//             import atlas.vulkan
-//         jolt_cpp
-//             import atlas.jolt_cpp
-//         ....
-//     physics
-//         import atlas.physics
-//     renderer
-//         import atlas.renderer
-// */
-
-// /*
-// core: module that is partitioned associated with
-//     logger
-//     import logger;
-// drivers:
-//     vulkan: module that is partitioned associated with
-//         import vulkan;
-//             internal: export module vulkan:*
-//     jolt_cpp: module that is partitioned associated with
-//         import jolt_cpp
-//             internal: export module jolt_cpp:*
-// atlas: global module unit
-//     export import core;
-//     export import vulkan;
-//     export import jolt_cpp
-// */
-
-int main() {
+int
+main() {
     atlas::console_log_manager manager = atlas::console_log_manager();
 
-    if(!glfwInit()) {
+    if (!glfwInit()) {
         console_log_fatal("GLFW: Initialization failed!!");
         return -1;
     }
 
     // TODO: Level streamer is going to be replacing system_registry
-    atlas::ref<atlas::system_registry> system = atlas::create_ref<atlas::system_registry>("system");
-    atlas::ref<atlas::graphics_context> context = atlas::initialize_context("vulkan", atlas::graphics_api::vulkan);
+    atlas::ref<atlas::system_registry> system =
+      atlas::create_ref<atlas::system_registry>("system");
+    atlas::ref<atlas::graphics_context> context =
+      atlas::initialize_context("vulkan", atlas::graphics_api::vulkan);
 
     atlas::ref<atlas::application> app = initialize_application(context);
 
     app->execute();
-    
+
     app->post_destroy();
 
     context->destroy();
--- editor/application.cpp
+++ editor/application.cpp
@@ -8,23 +8,26 @@ import atlas.drivers.graphics_context;
 
 class test_application : public atlas::application {
 public:
-    test_application(atlas::ref<atlas::graphics_context> p_context, const atlas::application_settings& p_settings) : atlas::application(p_context, p_settings) {
-        // atlas::register_update(this, &test_application::on_update);std::pmr::monotonic_buffer_resource resource{ 4096 };
-        // m_allocator.construct(&resource);
+    test_application(atlas::ref<atlas::graphics_context> p_context,
+                     const atlas::application_settings& p_settings)
+      : atlas::application(p_context, p_settings) {
+        // atlas::register_update(this,
+        // &test_application::on_update);std::pmr::monotonic_buffer_resource
+        // resource{ 4096 }; m_allocator.construct(&resource);
 
         // TODO -- this is going to be changed with the use of the level
         // streamer API
-        m_world = atlas::create_ref<editor_world>("Editor World", renderer_instance());
+        m_world =
+          atlas::create_ref<editor_world>("Editor World", renderer_instance());
     }
-
 
 private:
     // std::pmr::polymorphic_allocator<uint8_t> m_allocator;
     atlas::ref<editor_world> m_world;
-
 };
 
-atlas::ref<atlas::application> initialize_application(atlas::ref<atlas::graphics_context> p_contetxt) {
+atlas::ref<atlas::application>
+initialize_application(atlas::ref<atlas::graphics_context> p_contetxt) {
     atlas::application_settings settings = {
         .name = "Editor",
         .width = 1200,
